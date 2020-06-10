f8914a0a6f9c0b97acde99f2050c5720056e0466
celyes <celyes02@gmail.com>
2020-06-10 14:04:28 +0100

    update files: config.php

--- config.php
+++ config.php
@@ -1,13 +1,18 @@
 <?php 
 
+// Appplication configuration
+/**
+ * @todo switch to .env 
+ */
+
 return [
     'database' => [
-        'name' => 'test',
-        'username' => 'root', 
-        'password' => 'ferrari17',
-        'connection' => 'mysql:host=127.0.0.1',
+        'name' => '', // db name
+        'username' => '', // db user  
+        'password' => '', // db password
+        'connection' => 'mysql:host=127.0.0.1', // PDO dsn
         'options' => [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION // error mode
         ]
     ]
 ];
